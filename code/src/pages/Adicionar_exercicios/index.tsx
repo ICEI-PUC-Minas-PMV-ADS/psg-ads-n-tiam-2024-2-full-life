8c803aa01d36f0e777d29882c11c11153c70a591
guilhermehbs <--global>
2024-12-07 14:54:59 -0300

    config: resolving conflicts

--- code/src/pages/Adicionar_exercicios/index.tsx
+++ code/src/pages/Adicionar_exercicios/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert, SafeAreaView } from 'react-native';
 import { BarraSuperior } from '../../components/BarraSuperior';
 import { CampoDeEntrada } from '../../components/CampoDeEntrada';
 import { listarAnatomias, listarExercicios, adicionarExercicio, adicionarRecomendacaoExercicio } from '../../services/exerciciosService';
